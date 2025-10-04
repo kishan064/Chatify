ba2b0b2de4e10a61e3298a97e4fd49722770e8f4
kishan kumar sahu <sahu.kishan0444@gmail.com>
2025-10-04 10:17:30 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 const app= express();
+const __dirname=path.resolve();
 
 
 const PORT=process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+//for deployment
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/chatApp/dist")));
+
+    app.get("*",(_,res)=>{
+      res.sendFile(path.join(__dirname,"../frontend/chatApp/dist/index.html"))
+    })
+
+}
 
 app.listen(PORT, () => console.log("Server is running on port: "+ PORT));
 
