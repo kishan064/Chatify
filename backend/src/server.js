42b88250d878186522e6c884291b93e1baa1807b
kishan kumar sahu <sahu.kishan0444@gmail.com>
2025-10-04 11:44:25 +0530

    path changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,10 +17,10 @@ app.use("/api/messages",messageRoutes);
 
 //for deployment
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/chatApp/dist")));
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
     app.get("*",(_,res)=>{
-      res.sendFile(path.join(__dirname,"../frontend/chatApp/dist/index.html"))
+      res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
     })
 
 }
