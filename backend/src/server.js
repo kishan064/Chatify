aa76bacb31e1c8d6a5e383f6fe6dd86ce3854f4b
kishan kumar sahu <sahu.kishan0444@gmail.com>
2025-10-18 19:21:21 +0530

    side bar updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,6 +17,13 @@ app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));  
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
